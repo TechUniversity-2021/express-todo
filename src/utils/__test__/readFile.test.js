fc6ac728e0f41132dadc28e1c4ace290036d3ce5
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-19 17:41:12 +0530

    refactor(utils): change from test to describe/it in readFile.test.js

--- src/utils/__test__/readFile.test.js
+++ src/utils/__test__/readFile.test.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const promisifyFs = require('../readFile');
 
-test("fread should resolve with value 'this is async'", () => {
-  jest.spyOn(fs, 'readFile')
-    .mockImplementation((path, options, callback) => {
-      callback(null, 'this is async');
-    });
-  return expect(promisifyFs.fread()).resolves.toBe('this is async');
-});
+describe('Todo Service ', () => {
+  it("fread should resolve with value 'this is async'", () => {
+    jest.spyOn(fs, 'readFile')
+      .mockImplementation((path, options, callback) => {
+        callback(null, 'this is async');
+      });
+    return expect(promisifyFs.fread()).resolves.toBe('this is async');
+  });
 
-test("fread should reject with error message 'error' ", () => {
-  jest.spyOn(fs, 'readFile')
-    .mockImplementation((path, options, callback) => {
-      callback(new Error('error'), null);
-    });
-  return expect(promisifyFs.fread()).rejects.toEqual(Error('error'));
+  it("fread should reject with error message 'error' ", () => {
+    jest.spyOn(fs, 'readFile')
+      .mockImplementation((path, options, callback) => {
+        callback(new Error('error'), null);
+      });
+    return expect(promisifyFs.fread()).rejects.toEqual(Error('error'));
+  });
 });
