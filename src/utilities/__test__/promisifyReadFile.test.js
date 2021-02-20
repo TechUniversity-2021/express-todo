3fe065d16c0188b20c4311bb017626ff0ba12a08
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-02-20 10:36:34 +0530

    refactor: Change the format of test cases of File read

--- src/utilities/__test__/promisifyReadFile.test.js
+++ src/utilities/__test__/promisifyReadFile.test.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 const fileOps = require('../promisifyReadFile');
 
-test('Should return the content of files', async () => {
-  jest.spyOn(fs, 'readFile')
-    .mockImplementation((fileContent, option, callback) => {
-      callback(null, 'abc');
-    });
-  const text = await fileOps.promisifyFs('../seed/fruits12.txt');
-  expect(text).toBe('abc');
-});
+describe('File read', () => {
+  it('should return the content of files', async () => {
+    jest.spyOn(fs, 'readFile')
+      .mockImplementation((fileContent, option, callback) => {
+        callback(null, 'abc');
+      });
+    const text = await fileOps.promisifyFs('../seed/fruits12.txt');
+    expect(text).toBe('abc');
+  });
 
-test('Should return no such file found', () => {
-  jest
-    .spyOn(fs, 'readFile')
-    .mockImplementation((fileContent, option, callback) => {
-      callback(new Error('No such file found'), null);
-    });
-  return expect(fileOps.promisifyFs('../seed/fruits12.txt')).rejects.toEqual(new Error('No such file found'));
+  it('should return no such file found', () => {
+    jest
+      .spyOn(fs, 'readFile')
+      .mockImplementation((fileContent, option, callback) => {
+        callback(new Error('No such file found'), null);
+      });
+    return expect(fileOps.promisifyFs('xxx')).rejects.toEqual(new Error('No such file found'));
+  });
 });
