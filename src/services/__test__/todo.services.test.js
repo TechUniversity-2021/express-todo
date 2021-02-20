3eb87fca77c198bc109c4a6bb1e8f055c08f30cd
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-02-20 11:29:33 +0530

    test(todo-service): Add test cases to check the response structure

---
+++ src/services/__test__/todo.services.test.js
@@ -0,0 +1,12 @@
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const fileRead = require('../../utilities/promisifyReadFile');
+const { getTodos } = require('../todo.services');
+
+describe('Get Todo', () => {
+  it('should return the list of todos', async () => {
+    const spyOnFileReadContent = jest.spyOn(fileRead, 'promisifyFs').mockImplementation(('abc.txt'));
+    spyOnFileReadContent.mockResolvedValue('1|lala|active');
+    const todoObjects = await getTodos();
+    expect(todoObjects).toEqual([{"id": "1", "status": "active", "todo": "lala"}]);
+  });
+});
