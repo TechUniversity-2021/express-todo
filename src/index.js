9b820fc342aed4b75a3abfd4e3cd94d4a5c35fa0
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-20 18:22:36 +0530

    feat(src): add todo router to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const env = require('dotenv');
 const { DEFAULT_PORT } = require('./constants/config');
+const { todoRouter } = require('./routes');
 
 const app = express();
 env.config();
@@ -9,6 +10,7 @@ const port = process.env.PORT || DEFAULT_PORT;
 app.get('/', (req, res) => {
   res.send('Todo App!');
 });
+app.use('/todo', todoRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
