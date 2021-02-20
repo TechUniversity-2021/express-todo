eee9c1dcae07217c6d3fb36f19a6c462df9b0c33
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-20 16:33:03 +0530

    feat(todo ):  add server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.use('/todo', routes.todoRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`);
