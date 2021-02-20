d664fbbcb4865326f40abbca446f1fb39a393138
Anukriti <anukritigarg13@gmail.com>
2021-02-20 23:22:34 +0530

    feat(todo-api): Promisified fs.appendFile

--- src/utilities/__tests__/fsFunctions.utilities.test.js
+++ src/utilities/__tests__/fsFunctions.utilities.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { readFile, readDir } = require('../fsFunctions.utilities');
+const { readFile, readDir, appendFile } = require('../fsFunctions.utilities');
 
 describe('Promisified Read File function', () => {
   it('should resolve with file contents', () => {
@@ -54,6 +54,28 @@ describe('Promisified Read Directory function', () => {
       });
     return expect(readDir('MOCK_DIRECTORY')).rejects.toEqual(
       new Error('Directory not found!'),
+    );
+  });
+});
+
+describe('Promisified Append File function', () => {
+  it('should append data and resolve with success message', () => {
+    jest
+      .spyOn(fs, 'appendFile')
+      .mockImplementation((file, data, option, callback) => {
+        callback(null);
+      });
+    return expect(appendFile('MOCK_FILE', 'MOCK_DATA')).resolves.toBe('Success');
+  });
+  test('should reject with an error object incase an error occures during appending data', () => {
+    const MOCK_ERROR = new Error('An error occured');
+    jest
+      .spyOn(fs, 'appendFile')
+      .mockImplementation((file, data, option, callback) => {
+        callback(MOCK_ERROR);
+      });
+    return expect(readFile('MOCK_FILE')).rejects.toEqual(
+      MOCK_ERROR,
     );
   });
 });
--- src/utilities/fsFunctions.utilities.js
+++ src/utilities/fsFunctions.utilities.js
@@ -20,7 +20,18 @@ const promisifyReadDir = (dirPath) => new Promise((resolve, reject) => {
   });
 });
 
+const promisifyAppendFile = (filePath, data) => new Promise((resolve, reject) => {
+  fs.appendFile(filePath, data, 'utf-8', (err) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve('Success');
+    }
+  });
+});
+
 module.exports = {
   readFile: promisifyReadFile,
   readDir: promisifyReadDir,
+  appendFile: promisifyAppendFile,
 };
