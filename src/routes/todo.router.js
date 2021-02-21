bf9d209f039e87fc57a6dd754b4291179819ab24
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-22 01:48:04 +0530

    feat(router): add post router to create new todo

--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllTodosHandler } = require('../handlers/todo.handler');
+const { getAllTodosHandler, createTodoHandler } = require('../handlers/todo.handler');
 
 const router = express.Router();
 
 router.get('', getAllTodosHandler);
+router.post('', createTodoHandler);
 
 module.exports = { router };
