a8963ff1597cfbf0a292e0668be9bc162fa561d7
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-22 07:50:08 +0530

    feat(todo): add create and update todo handlers

--- src/handlers/todo.handler.js
+++ src/handlers/todo.handler.js
@@ -5,6 +5,19 @@ const getTodos = async (req, res) => {
   res.status(200).send(todo);
 };
 
+const createTodo = async (req, res) => {
+  const ack = await todoService.createTodo(req.body);
+  res.status(200).send(ack);
+};
+
+const updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const ack = await todoService.updateTodo(id, req.body);
+  res.status(200).send(ack);
+};
+
 module.exports = {
   getTodos,
+  createTodo,
+  updateTodo,
 };
