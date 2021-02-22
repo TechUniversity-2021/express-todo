528456d65a9aae114bfb99b627f0cd78dfe25e12
Anukriti <anukritigarg13@gmail.com>
2021-02-22 12:01:38 +0530

    refactor(todo-api): Rename validation files and club them in a folder

--- src/validation/todo.validation/todo.validator.js
+++ src/validation/todo.validation/todo.validator.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
-const { postBodySchema, updateBodySchema } = require('./validation.schema');
+const { postBodySchema, updateBodySchema } = require('./todo.validation.schema');
 
 const postValidator = (req, res, next) => {
   const { body } = req;
   const { value, error } = postBodySchema.validate(body);
   if (error) {
     res.status(400).send('Bad Request!');
+    return;
   }
   if (!body.status) {
     req.body.status = 'incomplete';
