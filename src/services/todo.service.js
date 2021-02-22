94deedd8d23d1a63ff0b971afaded0c0eb1be6ee
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-22 07:49:34 +0530

    feat(todo): add create and update todo service

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -1,3 +1,5 @@
+const { v4: uuid } = require('uuid');
+
 const fileUtils = require('../utils/file-utils');
 
 const getTodos = async () => {
@@ -16,4 +18,26 @@ const getTodos = async () => {
   return todosObject;
 };
 
-module.exports = { getTodos };
+const createTodo = async (content) => {
+  const newTodo = `\n${uuid()}|${content.todo}|Active`;
+  const ack = await fileUtils.appendFile('resources/todos.txt', newTodo);
+  return ack;
+};
+
+const updateTodo = async (id, content) => {
+  const text = await fileUtils.getFileData('resources/todos.txt');
+  const todosLines = text.split('\n');
+  let result;
+  let flag = 0;
+  todosLines.forEach((line) => {
+    if (line.startsWith(id)) {
+      flag = 1;
+      result = text.replace(line, `${id}|${content.todo}|${content.status}`);
+      fileUtils.updateFile('resources/todos.txt', result);
+    }
+  });
+  if (flag === 1) return 'Updated';
+  return 'Todo not found';
+};
+
+module.exports = { getTodos, createTodo, updateTodo };
