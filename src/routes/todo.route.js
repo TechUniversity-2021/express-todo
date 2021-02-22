22d9336d29c6bd64c0e02bbdaf33ecbdb46f78b1
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-22 07:49:56 +0530

    feat(todo): add create and update todo routes

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -4,7 +4,8 @@ const todoHandler = require('../handlers/todo.handler');
 const todoRouter = express.Router();
 
 todoRouter.get('/', todoHandler.getTodos);
-
+todoRouter.post('/', todoHandler.createTodo);
+todoRouter.put('/:id', todoHandler.updateTodo);
 module.exports = {
   todoRouter,
 };
