ec9187a38d25b055bca42349e472eb589d3ca380
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-23 06:59:20 +0530

    feat(handler):  add deleteTodohandler which returns status code and action message

--- src/handlers/__test__/todo.handler.test.js
+++ src/handlers/__test__/todo.handler.test.js
@@ -1,8 +1,8 @@
-const { getAllTodosHandler, createTodoHandler } = require('../todo.handler');
+const { getAllTodosHandler, createTodoHandler, deleteTodoHandler } = require('../todo.handler');
 const service = require('../../services/todo.service');
 
 describe('getAllTodos Handler', () => {
-  it('should return response with status code and all todo data', async () => {
+  it('should return response with todo data', async () => {
     const mockSend = jest.fn();
     const mockResponse = {
       status: jest.fn(() => ({ send: mockSend })),
@@ -20,6 +20,21 @@ describe('getAllTodos Handler', () => {
 
     expect(mockResponse.status).toHaveBeenCalledWith(200);
     expect(mockSend).toHaveBeenCalledWith(mockReturnObject);
+  });
+
+  it('should go to catch block', async () => {
+    const mockSend = jest.fn();
+    const mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
+    };
+
+    const getAllTodosServiceSpy = jest.spyOn(service, 'getAllTodos');
+    getAllTodosServiceSpy.mockImplementation(() => { throw new Error('error'); });
+
+    await getAllTodosHandler(null, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockSend).toHaveBeenCalledWith();
   });
 });
 
@@ -42,5 +57,62 @@ describe('createTodoHandler Handler', () => {
     await createTodoHandler(MockRequest, mockResponse);
     expect(mockResponse.status).toHaveBeenCalledWith(mockReturnObject.status);
     expect(mockSend).toHaveBeenCalledWith(mockReturnObject.message);
+  });
+});
+
+describe('updateTodoHandler Handler', () => {
+  it('should return response with status code and message', async () => {
+    const MockRequest = {
+      body: {},
+    };
+    const mockSend = jest.fn();
+    const mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
+    };
+    const mockReturnObject = {
+      status: 200,
+      message: 'finish',
+    };
+
+    jest.spyOn(service, 'createTodo').mockResolvedValue(mockReturnObject);
+
+    await createTodoHandler(MockRequest, mockResponse);
+    expect(mockResponse.status).toHaveBeenCalledWith(mockReturnObject.status);
+    expect(mockSend).toHaveBeenCalledWith(mockReturnObject.message);
+  });
+});
+
+describe('deleteTodos Handler', () => {
+  it('should return response with todo data', async () => {
+    const mockRequest = {
+      query: {
+        id: '1',
+      },
+    };
+    const mockSend = jest.fn();
+    const mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
+    };
+
+    jest.spyOn(service, 'deleteTodoByID').mockResolvedValue();
+
+    await deleteTodoHandler(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockSend).toHaveBeenCalledWith('todo deleted Successfully');
+  });
+
+  it('should go to catch block', async () => {
+    const mockSend = jest.fn();
+    const mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
+    };
+
+    jest.spyOn(service, 'getAllTodos').mockImplementation(() => { throw new Error('error'); });
+
+    await getAllTodosHandler(null, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockSend).toHaveBeenCalledWith();
   });
 });
--- src/handlers/todo.handler.js
+++ src/handlers/todo.handler.js
@@ -1,15 +1,38 @@
 const service = require('../services/todo.service');
 
 const getAllTodosHandler = async (req, res) => {
-  const tasksData = await service.getAllTodos();
-  res.status(200).send(tasksData);
+  try {
+    const tasksData = await service.getAllTodos();
+    res.status(200).send(tasksData);
+  } catch (error) {
+    res.status(500).send();
+  }
 };
 
 const createTodoHandler = async (req, res) => {
   const { body } = req;
+
   const response = await service.createTodo(body.title, body.status);
   res.status(response.status).send(response.message);
-  res.status(200).send();
 };
 
-module.exports = { getAllTodosHandler, createTodoHandler };
+const updateTodoHandler = async (req, res) => {
+  const { body } = req;
+  const response = await service.updateTodo(req.query.id, body.title, body.status);
+  res.status(response.status).send(response.message);
+};
+
+const deleteTodoHandler = async (req, res) => {
+  try {
+    const { query } = req;
+    await service.deleteTodoByID(query.id);
+    res.status(200).send('todo deleted Successfully');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+};
+
+module.exports = {
+  getAllTodosHandler, createTodoHandler, updateTodoHandler, deleteTodoHandler,
+};
