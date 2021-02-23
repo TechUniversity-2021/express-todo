f35422106a1412a061245dcb068b0dd53b80acb4
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-23 09:08:42 +0530

    add(routes):add routers for accessing database

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -4,10 +4,12 @@ const todoHandler = require('../handlers/todo.handlers');
 const todoRouter = express.Router();
 
 todoRouter.get('/', todoHandler.getTodosHandler);
-todoRouter.post('/',todoHandler.createTodo)
+todoRouter.post('/',todoHandler.createTodoHandler)
 todoRouter.put('/:id',todoHandler.updateTodoHandler)
 todoRouter.get('/:id',todoHandler.getTodosHandlerById)
 todoRouter.delete('/:id', todoHandler.deleteByIdToDoHandler);
+todoRouter.delete('/', todoHandler.deleteAllToDoHandler);
+todoRouter.delete('/:status', todoHandler.deleteByStatusToDoHandler);
 module.exports = {
   todoRouter,
 };
