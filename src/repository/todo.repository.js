680c5823c243599ea3758fa1eee83dc4a4d581bb
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-23 09:29:11 +0530

    feat(respository): add insertTodo, updateTodo and deleteTodo queries

--- src/repository/todo.repository.js
+++ src/repository/todo.repository.js
@@ -6,5 +6,18 @@ const getTodoByID = async (db, id) => {
   const todos = await db.query(`SELECT * FROM todos where id=${id}`);
   return todos.rows;
 };
+const insertTodo = async (db, title, status) => {
+  await db.query(`INSERT INTO todos(title,status)VALUES('${title}','${status}')`);
+};
 
-module.exports = { getAllTodos, getTodoByID };
+const updateTodo = async (db, id, title, status) => {
+  await db.query(`UPDATE todos SET title='${title}', status='${status}' WHERE id=${id}`);
+};
+
+const deleteTodoByID = async (db, id) => {
+  await db.query(`DELETE FROM todos WHERE id=${id}`);
+};
+
+module.exports = {
+  getAllTodos, getTodoByID, insertTodo, updateTodo, deleteTodoByID,
+};
