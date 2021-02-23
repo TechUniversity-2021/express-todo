0d16ae2e151d5b8f16ee5c3d2328f86ca5ec49c1
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-23 07:10:18 +0530

    test(todo-services): add get todos service test

--- src/services/todo.service.test.js
+++ src/services/todo.service.test.js
@@ -1,14 +1,21 @@
+const todoRepository = require('../repository/todo.repository');
 const fileUtils = require('../utils/file.utils');
 const todoService = require('./todo.service');
-
 // mockImplementation callbacks
 // mockresolved promises
 
 describe('Todo service', () => {
-  it('should return a list of todos when file read is successful', async () => {
-    jest.spyOn(fileUtils, 'getFileData').mockResolvedValue('1|break|active\n2|break|active');
-    const response = await todoService.getTodos();
-    expect(response).toStrictEqual([{ id: '1', todo: 'break', status: 'active' }, { id: '2', todo: 'break', status: 'active' }]);
+  it('should return a list of todos when db execution is successful', async () => {
+    const mockResponse = {
+      id: 1,
+      title: 'Work',
+      status: 'Active',
+      created_at: '3-5-8',
+      updated_at: null,
+    };
+    jest.spyOn(todoRepository, 'getTodos').mockResolvedValue(mockResponse);
+    const response = await todoService.getTodos('db');
+    expect(response).toStrictEqual(mockResponse);
   });
 
   it('should return a todo with id when file read is successful', async () => {
