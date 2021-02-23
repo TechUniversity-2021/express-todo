12186c29a38f537c753c54ce9ffd7c7cc30baf11
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-23 06:57:51 +0530

    feat(router): add delete router to delete a todo

--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getAllTodosHandler, createTodoHandler } = require('../handlers/todo.handler');
+const {
+  getAllTodosHandler, createTodoHandler, updateTodoHandler, deleteTodoHandler,
+} = require('../handlers/todo.handler');
 
 const router = express.Router();
 
 router.get('', getAllTodosHandler);
 router.post('', createTodoHandler);
+router.put('', updateTodoHandler);
+router.delete('', deleteTodoHandler);
 
 module.exports = { router };
