3a824ced55975268817e44611217c08e92bf5b4f
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-23 07:24:32 +0530

    feat(src): setup database connection using postgres pool

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const env = require('dotenv');
+const { Pool } = require('pg');
+const { dbConfig } = require('./constants/db.config');
 const { DEFAULT_PORT } = require('./constants/config');
 const { todoRouter } = require('./routes');
 
 const app = express();
+const pool = new Pool(dbConfig);
+
 env.config();
 const port = process.env.PORT || DEFAULT_PORT;
+
 app.use(express.json());
+app.locals.db = pool;
 app.get('/', (req, res) => {
   res.send('Todo App!');
 });
