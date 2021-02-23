fe6b4f593357192a5b706e5697213a343ebd542a
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-23 06:14:51 +0530

    chore: setup package.json and add index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const routes = require('./routes/index');
+const { Pool } = require('pg');
+const { dbConfig } = require('./config/db.config.js')
 const app = express();
 const port = process.env.PORT || 8080;
-
+const pool =new Pool(dbConfig);
 app.use(express.json());
 
+app.use('/todo', routes.todoRouter);
+app.locals.db=pool;
 app.listen(port, () => {
+  
   console.log(`Server listening at port: ${port}`);
 });
