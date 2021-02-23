9bc8e9bb8f75005a74c8a9258f45bdd8cd1003a5
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-23 06:10:13 +0530

    feat(routes): add routes to get,create,update and delete todos

---
+++ src/routes/todo.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const todoHandler = require('../handlers/todo.handlers');
+
+const todoRouter = express.Router();
+
+todoRouter.get('/', todoHandler.getTodosHandler);
+todoRouter.post('/',todoHandler.createTodo)
+todoRouter.put('/:id',todoHandler.updateTodoHandler)
+todoRouter.get('/:id',todoHandler.getTodosHandlerById)
+todoRouter.delete('/:id', todoHandler.deleteByIdToDoHandler);
+module.exports = {
+  todoRouter,
+};
