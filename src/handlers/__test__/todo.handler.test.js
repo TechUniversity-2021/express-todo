781aeef7efed520775883b86d91510881e1a519d
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-23 08:14:25 +0530

    feat(handler): add support for getTodoByID inside getAllTodosHandler and update test as well

--- src/handlers/__test__/todo.handler.test.js
+++ src/handlers/__test__/todo.handler.test.js
@@ -4,12 +4,15 @@ const {
 const service = require('../../services/todo.service');
 
 describe('getAllTodos Handler', () => {
-  it('should return response with todo data', async () => {
+  it('should go to getAllTodos service return response with todo data', async () => {
     const mockRequestObject = {
       app: {
         locals: {
           db: {},
         },
+      },
+      query: {
+
       },
     };
     const mockSend = jest.fn();
@@ -30,12 +33,15 @@ describe('getAllTodos Handler', () => {
     expect(mockSend).toHaveBeenCalledWith(mockReturnObject);
   });
 
-  it('should go to catch block', async () => {
+  it('should go to getAllTodos service and enter catch block', async () => {
     const mockRequestObject = {
       app: {
         locals: {
           db: {},
         },
+      },
+      query: {
+
       },
     };
     const mockSend = jest.fn();
@@ -45,6 +51,58 @@ describe('getAllTodos Handler', () => {
 
     const getAllTodosServiceSpy = jest.spyOn(service, 'getAllTodos');
     getAllTodosServiceSpy.mockImplementation(() => { throw new Error('error'); });
+
+    await getAllTodosHandler(mockRequestObject, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockSend).toHaveBeenCalledWith();
+  });
+  it('should go to getTodoByID service return response with todo data', async () => {
+    const mockRequestObject = {
+      app: {
+        locals: {
+          db: {},
+        },
+      },
+      query: {
+        id: 1,
+      },
+    };
+    const mockSend = jest.fn();
+    const mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
+    };
+    const mockReturnObject = [{
+      id: 1,
+      title: 'coding',
+      staus: 'active',
+    }];
+
+    jest.spyOn(service, 'getTodoByID').mockResolvedValue(mockReturnObject);
+
+    await getAllTodosHandler(mockRequestObject, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockSend).toHaveBeenCalledWith(mockReturnObject);
+  });
+
+  it('should go to getTodoByID service and enter catch block', async () => {
+    const mockRequestObject = {
+      app: {
+        locals: {
+          db: {},
+        },
+      },
+      query: {
+        id: 1,
+      },
+    };
+    const mockSend = jest.fn();
+    const mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
+    };
+
+    jest.spyOn(service, 'getTodoByID').mockImplementation(() => { throw new Error('error'); });
 
     await getAllTodosHandler(mockRequestObject, mockResponse);
 
--- src/handlers/todo.handler.js
+++ src/handlers/todo.handler.js
@@ -3,8 +3,14 @@ const service = require('../services/todo.service');
 const getAllTodosHandler = async (req, res) => {
   try {
     const { db } = req.app.locals;
-    const todos = await service.getAllTodos(db);
-    res.status(200).send(todos);
+    const { query } = req;
+    if (query.id === undefined) {
+      const todos = await service.getAllTodos(db);
+      res.status(200).send(todos);
+    } else {
+      const todo = await service.getTodoByID(db, query.id);
+      res.status(200).send(todo);
+    }
   } catch (error) {
     res.status(500).send();
   }
